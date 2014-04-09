05b46542aab76ea59203697eb78c4fa9acf8103a
jackdaniels1137 <jackdaniels11153@gmail.com>
2014-04-09 20:53:39 -0300

    creacion ventana

--- laboratorio/src/laboratorio/Ventana.java
+++ laboratorio/src/laboratorio/Ventana.java
@@ -1,5 +1,14 @@
 package laboratorio;
 
+import javax.swing.JFrame;
+
 public class Ventana {
 
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("My first windows");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		}
 }
