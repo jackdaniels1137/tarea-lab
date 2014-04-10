0384accec94b9d4eb0efb846037c8a7aa8deb37c
jackdaniels1137 <jackdaniels11153@gmail.com>
2014-04-09 21:01:43 -0300

    creacion label

--- laboratorio/src/laboratorio/Ventana.java
+++ laboratorio/src/laboratorio/Ventana.java
@@ -1,11 +1,15 @@
 package laboratorio;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Ventana {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		JLabel label = new JLabel();
+		label.setText("Holaaa soy un Label");
+		frame.add(label);
 		frame.setTitle("My first windows");
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
