cdd40704713790dbda27587a5cf9b518b04355ed
jackdaniels1137 <jackdaniels11153@gmail.com>
2014-04-09 21:05:38 -0300

    creacion del button

--- laboratorio/src/laboratorio/Ventana.java
+++ laboratorio/src/laboratorio/Ventana.java
@@ -1,15 +1,33 @@
 package laboratorio;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class Ventana {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		
 		JLabel label = new JLabel();
 		label.setText("Holaaa soy un Label");
 		frame.add(label);
+		
+		JButton button = new JButton();
+		button.setText("Presioname pls");
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(null, "Finished");
+					}
+			}
+		);
+		frame.add(button);
+		
 		frame.setTitle("My first windows");
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
